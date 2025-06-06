6fe3656245400b49d4650f2d9885481da5cc6a3c
Jarne-Wils <jarne.wils@flux.be>
2025-06-07 00:10:55 +0200

    map2

--- scripts/main.js
+++ scripts/main.js
@@ -212,7 +212,7 @@ orbitCamera.position.set(25, 20, 25);
 
 // scene
 const scene = new THREE.Scene();
-let selectedMap = 'map1';
+let selectedMap = 'map2';
 let world = new World(selectedMap, socket);
 world.generate();
 scene.add(world);
