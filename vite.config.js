d3685276609ae282744545d8c5a96b33d522089c
Jarne-Wils <jarne.wils@flux.be>
2025-06-09 01:04:32 +0200

    page bug fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
 /**
- * @type {import { 'vite' }.UserConfig}
+ * @type {import('vite').UserConfig}
  */
-export default {
+export default defineConfig({
   build: {
     sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        game: resolve(__dirname, 'game.html'),
+      },
+    },
   },
-};
+});
